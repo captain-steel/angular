1e6e9e68508fc1ca75d9f66c8356ad0f0e66cb4b
Wassim AZIRAR <wassim.azirar@gmail.com>
2020-09-23 16:18:38 +0200

    added student manually to app.component and displayed them into html page

--- src/app/components/students/students.component.ts
+++ src/app/components/students/students.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Student } from '../../models/student.model';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.scss']
 })
-export class StudentsComponent implements OnInit {
-  constructor() {}
+export class StudentsComponent {
+  studentsList: Student[];
 
-  ngOnInit(): void {}
+  constructor() {
+    this.studentsList = [
+      {
+        name: 'Aurélie',
+        age: 22,
+        picture: 'https://randomuser.me/api/portraits/women/63.jpg'
+      },
+      {
+        name: 'Gerard',
+        age: 21,
+        picture: 'https://randomuser.me/api/portraits/men/20.jpg'
+      },
+      {
+        name: 'Stéphanie',
+        age: 29,
+        picture: 'https://randomuser.me/api/portraits/women/11.jpg'
+      }
+    ];
+  }
 }
